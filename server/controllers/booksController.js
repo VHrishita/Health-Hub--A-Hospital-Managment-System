9840fdd4ed123180c3ceef4588d8832396db9a17
Vempali Hrishita <vempalihrishita05@gmail.com>
2025-12-13 17:16:54 +0530

    Update booksController.js

--- server/controllers/booksController.js
+++ server/controllers/booksController.js
@@ -1,6 +1,5 @@
 import db from "../db.js";
 
-// Get all books
 export const getBooks = (req, res) => {
   db.query("SELECT * FROM books", (err, results) => {
     if (err) return res.status(500).json(err);

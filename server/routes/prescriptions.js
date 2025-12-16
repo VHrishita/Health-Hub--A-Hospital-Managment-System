afc29e807f1e453a201fdb69faede83b8d99ee74
Vempali Hrishita <vempalihrishita05@gmail.com>
2025-12-16 20:52:44 +0530

    Update prescriptions.js

--- server/routes/prescriptions.js
+++ server/routes/prescriptions.js
@@ -16,8 +16,6 @@ router.post('/add', async (req, res) => {
         res.status(500).json({ success: false, error: err.message });
     }
 });
-
-// Get all prescriptions
 router.get('/all', async (req, res) => {
     const [rows] = await db.execute('SELECT * FROM prescriptions');
     res.json(rows);

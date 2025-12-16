25bee9828a86e3267ad564e9c4218995c5aec4ba
Vempali Hrishita <vempalihrishita05@gmail.com>
2025-12-16 20:52:13 +0530

    Update patients.js

--- server/routes/patients.js
+++ server/routes/patients.js
@@ -2,8 +2,6 @@ import express from 'express';
 import db from '../db.js';
 const router = express.Router();
 const patients = await fetch("https://health-hub-a-hospital-managment-system.onrender.com/api/patients/all");
-
-// Add patient
 router.post('/add', async (req, res) => {
     const { hospital_id, first_name, last_name, dob, gender, blood_group, contact, address, emergency_contact, patient_type } = req.body;
     try {

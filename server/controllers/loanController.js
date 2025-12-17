3c3d505f717b451d812a71bd3bb17585056412f1
Vempali Hrishita <vempalihrishita05@gmail.com>
2025-12-17 23:23:52 +0530

    Update loanController.js

--- server/controllers/loanController.js
+++ server/controllers/loanController.js
@@ -1,10 +1,1 @@
 
-export const getLoanById = (req, res) => {
-  const { id } = req.params;
-  db.query("SELECT * FROM loan WHERE loan_id=?", [id], (err, results) => {
-    if (err) return res.status(500).json(err);
-    res.json(results[0]);
-  });
-};
-
-Loan = (req, res) =

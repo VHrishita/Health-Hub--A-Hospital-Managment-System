968ebe35e169759432f422252d25336637a018da
Vempali Hrishita <vempalihrishita05@gmail.com>
2025-12-17 23:24:07 +0530

    Update membersController.js

--- server/controllers/membersController.js
+++ server/controllers/membersController.js
@@ -1,23 +1,4 @@
 
-
-export const getMemberById = (req, res) => {
-  const { id } = req.params;
-  db.query("SELECT * FROM members WHERE member_id = ?", [id], (err, results) => {
-    if (err) return res.status(500).json(err);
-    res.json(results[0]);
-  });
-};
-export const createMember = (req, res) => {
-  const { name, email, phone, membership_date } = req.body;
-  db.query(
-    "INSERT INTO members (name, email, phone, membership_date) VALUES (?, ?, ?, ?)",
-    [name, email, phone, membership_date],
-    (err, result) => {
-      if (err) return res.status(500).json(err);
-      res.json({ message: "Member created", member_id: result.insertId });
-    }
-  );
-};
 export const updateMember = (req, res) => {
   const { id } = req.params;
   const { name, email, phone, membership_date } = req.body;

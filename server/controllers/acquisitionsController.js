ea75ff5bfb0bd52042a51ada731906bce04c318a
Vempali Hrishita <vempalihrishita05@gmail.com>
2025-12-18 14:52:03 +0530

    Update acquisitionsController.js

--- server/controllers/acquisitionsController.js
+++ server/controllers/acquisitionsController.js
@@ -1,9 +1,1 @@
 
-
-export const deleteAcquisition = (req, res) => {
-  const { id } = req.params;
-  db.query("DELETE FROM acquisitions WHERE acquisition_id=?", [id], (err) => {
-    if (err) return res.status(500).json(err);
-    res.json({ message: "Acquisition deleted" });
-  });
-};

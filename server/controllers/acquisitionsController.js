b418dc4c5c71c1c7daabb2342a63caf54ae6b80c
Vempali Hrishita <vempalihrishita05@gmail.com>
2025-12-18 14:51:49 +0530

    Update acquisitionsController.js

--- server/controllers/acquisitionsController.js
+++ server/controllers/acquisitionsController.js
@@ -1,25 +1,4 @@
 
-
-export const getAcquisitionById = (req, res) => {
-  const { id } = req.params;
-  db.query("SELECT * FROM acquisitions WHERE acquisition_id=?", [id], (err, results) => {
-    if (err) return res.status(500).json(err);
-    res.json(results[0]);
-  });
-};
-
-export const createAcquisition = (req, res) => {
-  const { book_id, supplier_id, purchase_date, cost } = req.body;
-  db.query(
-    "INSERT INTO acquisitions (book_id, supplier_id, purchase_date, cost) VALUES (?, ?, ?, ?)",
-    [book_id, supplier_id, purchase_date, cost],
-    (err, result) => {
-      if (err) return res.status(500).json(err);
-      res.json({ message: "Acquisition created", acquisition_id: result.insertId });
-    }
-  );
-};
-
 export const updateAcquisition = (req, res) => {
   const { id } = req.params;
   const { book_id, supplier_id, purchase_date, cost } = req.body;

a5ed49754762d317986565a63530a8e9924336f3
Vempali Hrishita <vempalihrishita05@gmail.com>
2025-12-18 14:50:51 +0530

    Update booksController.js

--- server/controllers/booksController.js
+++ server/controllers/booksController.js
@@ -1,18 +1,5 @@
 
 
-export const updateBook = (req, res) => {
-  const { id } = req.params;
-  const { title, author, genre, year_published, available_copies } = req.body;
-  db.query(
-    "UPDATE books SET title=?, author=?, genre=?, year_published=?, available_copies=? WHERE book_id=?",
-    [title, author, genre, year_published, available_copies, id],
-    (err) => {
-      if (err) return res.status(500).json(err);
-      res.json({ message: "Book updated" });
-    }
-  );
-};
-
 export const deleteBook = (req, res) => {
   const { id } = req.params;
   db.query("DELETE FROM books WHERE book_id = ?", [id], (err) => {

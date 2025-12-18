7d289c01b853742e8a4df02fd8fdb23fd859b672
Vempali Hrishita <vempalihrishita05@gmail.com>
2025-12-18 14:50:44 +0530

    Remove unused book controller functions
    Removed getBookById and createBook functions from booksController.

--- server/controllers/booksController.js
+++ server/controllers/booksController.js
@@ -1,22 +1,4 @@
 
-export const getBookById = (req, res) => {
-  const { id } = req.params;
-  db.query("SELECT * FROM books WHERE book_id = ?", [id], (err, results) => {
-    if (err) return res.status(500).json(err);
-    res.json(results[0]);
-  });
-};
-export const createBook = (req, res) => {
-  const { title, author, genre, year_published, available_copies } = req.body;
-  db.query(
-    "INSERT INTO books (title, author, genre, year_published, available_copies) VALUES (?, ?, ?, ?, ?)",
-    [title, author, genre, year_published, available_copies],
-    (err, result) => {
-      if (err) return res.status(500).json(err);
-      res.json({ message: "Book created", book_id: result.insertId });
-    }
-  );
-};
 
 export const updateBook = (req, res) => {
   const { id } = req.params;

1c1c3f00a52e5700ef195185e2915f95e6ce9012
Vempali Hrishita <vempalihrishita05@gmail.com>
2025-12-18 14:51:57 +0530

    Update acquisitionsController.js

--- server/controllers/acquisitionsController.js
+++ server/controllers/acquisitionsController.js
@@ -1,16 +1,4 @@
 
-export const updateAcquisition = (req, res) => {
-  const { id } = req.params;
-  const { book_id, supplier_id, purchase_date, cost } = req.body;
-  db.query(
-    "UPDATE acquisitions SET book_id=?, supplier_id=?, purchase_date=?, cost=? WHERE acquisition_id=?",
-    [book_id, supplier_id, purchase_date, cost, id],
-    (err) => {
-      if (err) return res.status(500).json(err);
-      res.json({ message: "Acquisition updated" });
-    }
-  );
-};
 
 export const deleteAcquisition = (req, res) => {
   const { id } = req.params;

6a84b948be98cda78f135da5b851ebee44582fa5
Vempali Hrishita <vempalihrishita05@gmail.com>
2025-12-18 14:50:58 +0530

    Remove deleteBook function
    Remove the deleteBook function from booksController.

--- server/controllers/booksController.js
+++ server/controllers/booksController.js
@@ -1,9 +1,1 @@
 
-
-export const deleteBook = (req, res) => {
-  const { id } = req.params;
-  db.query("DELETE FROM books WHERE book_id = ?", [id], (err) => {
-    if (err) return res.status(500).json(err);
-    res.json({ message: "Book deleted" });
-  });
-};
